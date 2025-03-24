f4b1fcc8f75c9e914b78e6feb98f6adfc01f64f7
beranekleon <leon.beranek06@gmail.com>
2025-03-24 09:29:03 +0100

    create: create project for warehouse management software

---
+++ software/WarehouseManagementProgram.Console/WarehouseManagementProgram.Console/Product.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+public class Product
+{
+    public string Name { get; set; }
+
+    //[JsonIgnore] --> can be used to ignore a property
+    public int Price { get; set; }
+}
+
---
+++ software/WarehouseManagementProgram.Console/WarehouseManagementProgram.Console/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Product[] products = new Product[]
+        {
+            new Product { Name = "Laptop", Price = 1000 },
+            new Product { Name = "Mouse", Price = 20 },
+            new Product { Name = "Keyboard", Price = 50 }
+        };
+
+        var options = new JsonSerializerOptions { WriteIndented = true };
+        string json = JsonSerializer.Serialize(products,options);
+
+        string filePath = "products.json";
+        File.WriteAllText(filePath, json);
+        Console.WriteLine("JSON saved to:" + Path.GetFullPath(filePath));
+    }
+}
+
